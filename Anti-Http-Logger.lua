effab2439a973703c76bb85e5c00b4b2c0005225
vqmpjay <bakirbutkovic13@gmail.com>
2025-08-15 17:46:29 +0200

    Create Anti-Http-Logger.lua

---
+++ Anti-Http-Logger.lua
@@ -0,0 +1,30 @@
+--[[
+ANTI HTTP LOGGER
+Prevents all kinds of HTTP logging methods and is completely invisible and undetected.
+Should work on all executors, apart from Codex, since it tends to break on obfuscated code.
+
+Made with love, vqmpjay (NOT vinnie) <3
+-----------------------------------------------------------------------------------------------------
+ __   __   ______     _____     ______     __     ______   ______   ______    
+/\ \ / /  /\  __ \   /\  __-.  /\  == \   /\ \   /\  ___\ /\__  _\ /\  ___\   
+\ \ \'/   \ \  __ \  \ \ \/\ \ \ \  __<   \ \ \  \ \  __\ \/_/\ \/ \ \___  \  
+ \ \__|    \ \_\ \_\  \ \____-  \ \_\ \_\  \ \_\  \ \_\      \ \_\  \/\_____\ 
+  \/_/      \/_/\/_/   \/____/   \/_/ /_/   \/_/   \/_/       \/_/   \/_____/ 
+                                dsc.gg/vadriftz
+-----------------------------------------------------------------------------------------------------
+
+* Note: the script is obfuscated so that there's less of a chance of it getting bypassed. Hope you understand.
+Features:
+- Reliable and silent anti-HTTP logger
+- Never seen before method
+- Quick termination and kick-on detection
+- Quick detection for clipboards, files, console, rconsole*? and GUIs of all types
+- Compatible with a lot of executors, including Xeno/Solara (*may lag a bit on these executors)
+- Constant monitoring
+- Prevents Function hook restoration and tampering
+
+(It's hard to explain how advanced the script is without saying how it works directly)
+]]
+
+--obfuscation method @ .gg/25ms
+-- bot down, script will be back soon
